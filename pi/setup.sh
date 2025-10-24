5ae6e8eab7586e8a2c5bfedc9f568fc1a6a4822f
Yngve Svalestuen <yngve@outlook.com>
2025-10-24 13:29:29 +0200

    Run zigbee2mqtt

--- pi/setup.sh
+++ pi/setup.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 set -xe
 
+USB_NAME="usb-1a86_USB_Serial-if00-port0"
+docker run \
+   --name zigbee2mqtt \
+   --restart=unless-stopped \
+   --device="/dev/serial/by-id/$USB_NAME:/dev/ttyACM0" \
+   -p 8080:8080 \
+   -v $(pwd)/data:/app/data \
+   -v /run/udev:/run/udev:ro \
+   -e TZ=Europe/Amsterdam \
+   -d \
+   ghcr.io/koenkk/zigbee2mqtt
+
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 OUTPUT_DIR="$HOME/office-iot-output"
-echo "hello $TIMESTAMP" >> "$OUTPUT_DIR/output.txt"
-
+echo "setup ran at $TIMESTAMP" >> "$OUTPUT_DIR/output.txt"
