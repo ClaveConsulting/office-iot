cf400a064c4ff98d9c4c026d6741cf03081d366e
Yngve Svalestuen <yngve@outlook.com>
2025-11-21 12:07:51 +0100

    add empty mosquitto config file

--- pi/setup.sh
+++ pi/setup.sh
@@ -3,6 +3,9 @@ set -xe
 
 mkdir -p $HOME/office-iot-output/zigbee2mqtt/data
 mkdir -p $HOME/office-iot-output/eclipse-mosquitto/data
+mkdir -p $HOME/office-iot-output/eclipse-mosquitto/config
+mkdir -p $HOME/office-iot-output/eclipse-mosquitto/log
 cp ./zigbee2mqtt/* $HOME/office-iot-output/zigbee2mqtt/data
+cp ./eclipse-mosquitto/* $HOME/office-iot-output/eclipse-mosquitto/config
 
 docker compose up --detach 
