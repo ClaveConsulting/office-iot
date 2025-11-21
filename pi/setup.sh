68fe24fcf5eba35f75d78d0b0e26287c111195ee
Yngve Svalestuen <yngve@outlook.com>
2025-11-21 12:12:19 +0100

    fix path

--- pi/setup.sh
+++ pi/setup.sh
@@ -6,6 +6,6 @@ mkdir -p $HOME/office-iot-output/eclipse-mosquitto/data
 mkdir -p $HOME/office-iot-output/eclipse-mosquitto/config
 mkdir -p $HOME/office-iot-output/eclipse-mosquitto/log
 cp ./zigbee2mqtt/* $HOME/office-iot-output/zigbee2mqtt/data
-cp ./eclipse-mosquitto/* $HOME/office-iot-output/eclipse-mosquitto/config
+cp ./eclipse-mosquitto/config/* $HOME/office-iot-output/eclipse-mosquitto/config
 
 docker compose up --detach 
