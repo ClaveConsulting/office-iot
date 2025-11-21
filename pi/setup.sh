2a60e499e59117248864f4516b6d82bd34e2c333
Yngve Svalestuen <yngve@outlook.com>
2025-11-21 11:24:27 +0100

    copy config file instead of binding it

--- pi/setup.sh
+++ pi/setup.sh
@@ -3,4 +3,6 @@ set -xe
 
 mkdir -p $HOME/office-iot-output/zigbee2mqtt/data
 mkdir -p $HOME/office-iot-output/eclipse-mosquitto/data
+cp ./zigbee2mqtt/* $HOME/office-iot-output/zigbee2mqtt/data
+
 docker compose up --detach 
