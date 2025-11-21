c34f745a3fa159cf945505c04ce2c2217f20ecf8
Yngve Svalestuen <yngve@outlook.com>
2025-11-21 12:30:38 +0100

    cp with sudo

--- pi/setup.sh
+++ pi/setup.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -xe
 
-docker compose down
-
 mkdir -p $HOME/office-iot-output/zigbee2mqtt/data
 mkdir -p $HOME/office-iot-output/eclipse-mosquitto/data
 mkdir -p $HOME/office-iot-output/eclipse-mosquitto/config
 mkdir -p $HOME/office-iot-output/eclipse-mosquitto/log
-cp ./zigbee2mqtt/* $HOME/office-iot-output/zigbee2mqtt/data
-cp ./eclipse-mosquitto/config/* $HOME/office-iot-output/eclipse-mosquitto/config
+sudo cp ./zigbee2mqtt/* $HOME/office-iot-output/zigbee2mqtt/data
+sudo cp ./eclipse-mosquitto/config/* $HOME/office-iot-output/eclipse-mosquitto/config
 
 docker compose up --detach 
